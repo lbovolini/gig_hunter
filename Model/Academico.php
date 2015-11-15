edae48eec68d2e4f35e9253f0945975060024031
Lucas Bovolini <lbovolini94@gmail.com>
2015-11-15 00:00:09 -0200

    Corrigido

--- Model/Academico.php
+++ Model/Academico.php
@@ -15,31 +15,11 @@ class Academico
 	private $lattes;
 	private $linkedin;
 
-	public function __construct($nome, $email, $username, $senha, $data_nascimento, $telefone, $rg, $cpf, $instituicao, $matricula, $lattes, $linkedin)
-	{
-		$this->nome = $nome;
-		$this->email = $email;
-		$this->username = $username;
-		$this->senha = $senha;
-		$this->data_nascimento = $data_nascimento;
-		$this->telefone = $telefone;
-		$this->rg = $rg;
-		$this->cpf = $cpf;
-		$this->instituicao = $instituicao;
-		$this->matricula = $matricula;
-		$this->lattes = $lattes;
-		$this->linkedin = $linkedin;
-	}
+	public function __construct() {}
 
 	//Retorna dados de um Academico
 	public static function getAcademico($cpf) {
-		$query = "SELECT 1 FROM usuarios WHERE cpf = '" . $cpf . "' LIMIT 1";
-		$resultado = mysql_query($query);
-		return(mysql_fetch_array($resultado));
-	}
-	
-	public static function getAcademicoLogin($login, $senha) {
-		$query = "SELECT 1 FROM usuarios WHERE `tipo` = 'Academico' AND `username` = '".$login."' AND `senha` = '".$senha."' LIMIT 1";
+		$query = "SELECT id FROM usuarios WHERE cpf = '{$cpf}' LIMIT 1";
 		$resultado = mysql_query($query);
 		return(mysql_fetch_array($resultado));
 	}
@@ -49,20 +29,6 @@ class Academico
 	{
 		$query = "INSERT INTO usuarios(nome, email, username, senha, data_nascimento, telefone, rg, cpf, status, tempo_bloqueada, tipo, instituicao, matricula, lattes, linkedin, endereco_id) VALUES ('" . $nome . "', '" . $email . "', '" . $username . "', '" . $senha . "', '" . $data_nascimento . "', '" . $telefone . "', '" . $rg . "', '" . $cpf . "', 'Ativo', '0', 'Academico', '" . $instituicao . "', '" . $matricula . "', '" . $lattes . "', '" . $linkedin . "', '" . $idEndereco . "')";
 		mysql_query($query);
-	}
-
-	public static function validate($data)
-	{
-		foreach ($data as $field) {
-			  $field = trim($field);
-			  $field = stripslashes($field);
-			  $field = htmlspecialchars($field);
-
-			  //echo $field . '<br>';
-			  if (empty($field)) 
-			  	return true;
-		}
-		return false;
 	}
 }
 ?>
