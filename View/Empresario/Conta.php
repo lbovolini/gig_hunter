27f5630a84a1bc2890ee748f41d9a0cf8940060b
Lucas Bovolini <lbovolini94@gmail.com>
2015-12-03 00:24:26 -0200

    Data -> Text

--- View/Empresario/Conta.php
+++ View/Empresario/Conta.php
@@ -124,7 +124,7 @@ require $root.'/View/Templates/DefaultNav.php'; ?>
           <div class="form-group">
             <label class="col-sm-2 control-label">Data de Nascimento</label>
             <div class="col-md-3">
-            <input class="form-control" type="date" id="data_nascimento" name="data_nascimento" placeholder="Ex. 01/01/1999" value="<?php $data = implode("/",array_reverse(explode("-", $row["data_nascimento"]))); echo $data; ?>">
+            <input class="form-control" type="text" id="data_nascimento" name="data_nascimento" placeholder="Ex. 01/01/1999" value="<?php $data = implode("/",array_reverse(explode("-", $row["data_nascimento"]))); echo $data; ?>">
             </div>
             <label class="col-sm-2 control-label">Número de Telefone</label>
             <div class="col-md-3">
